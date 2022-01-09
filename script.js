d0bf1977bb6981f103c7a6b08f55058b5e16a64f
ahmed-nasir5020 <an0991382813@gmail.com>
2022-01-09 09:06:47 +0000

    my store

---
+++ script.js
@@ -0,0 +1,6 @@
+function showTab(tabID){
+	document.getElementById('elect').style.display="none"
+	document.getElementById('cloth').style.display="none"
+	document.getElementById('food').style.display="none"
+	document.getElementById(tabID).style.display="block"
+}
